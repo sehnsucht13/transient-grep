a8b66beb872da58dc47cc580ec5e751eb74fadb8
Yavor Konstantinov <ykonstantinov1@gmail.com>
2019-07-31 00:36:22 -0700

    Move the menu around so it looks better

--- transient-grep.el
+++ transient-grep.el
@@ -1,35 +1,46 @@
 (require 'transient)
 
 (define-infix-argument grep--transient-context-num:-A ()
-  :description "# of lines of trailing context"
+  :description "Num of lines of trailing context"
   :class 'transient-option
   :shortarg "-A"
   :argument "--after-context="
   )
 
 (define-infix-argument grep--transient-context-num:-B ()
-  :description "# of lines of leading context"
+  :description "Num of lines of leading context"
   :class 'transient-option
   :shortarg "-B"
   :argument "--before-context="
   )
 
 (define-infix-argument grep--transient-context-num:-C ()
-  :description "# of lines of output context"
+  :description "Num of lines of output context"
   :class 'transient-option
   :shortarg "-C"
   :argument "--context="
   )
 
-(define-transient-command grep-transient--menu ()
+(define-transient-command my-transient-menu ()
   "A transient menu for the build in grep command."
   :man-page "grep"
-  [["Matcher Pattern"
+  [
+   ["Matcher Pattern"
     ("-E" "Extended Regexps" ("-E" "--extended-regexp"))
     ("-F" "Fixed Strings" ("-F" "--fixed-strings"))
     ("-G" "Basic Regexps" ("-G" "--basic-regexp"))
     ("-P" "Perl-compatible Regexps" ("-P" "--perl-regexp"))
     ]
+   ["Matching Patterns"
+    ("-e" "Patterns" ("-e" "--regexp="))
+    ("-f" "File" ("-f" "--file"))
+    ("-i" "Ignore case" ("-i" "--ignore-case"))
+    ("-v" "Invert match" ("-v" "--invert-match"))
+    ("-w" "Only whole words" ("-w" "--word-regexp"))
+    ("-x" "Only whole lines" ("-x" "--line-regexp"))
+    ]
+   ]
+  [
    ["Output Control"
     ("-c" "# of matches" ("-c" "--count"))
     ("-L" "Print files with no matches" ("-L" "--files-without-match"))
@@ -39,18 +50,14 @@
     ("-s" "Suppress error messages" ("-s" "--no-messages"))
     ]
    ]
-  [
-   ["Matching Patterns"
-    ("-e" "Patterns" ("-e" "--regexp="))
-    ("-f" "File" ("-f" "--file"))
-    ("-i" "Ignore case" ("-i" "--ignore-case"))
-    ("-v" "Invert match" ("-v" "--invert-match"))
-    ("-w" "Only whole words" ("-w" "--word-regexp"))
-    ("-x" "Only whole lines" ("-x" "--line-regexp"))
-    ]
-   ["Other"
+  ["Context"
+   (grep--transient-context-num:-A)
+   (grep--transient-context-num:-B)
+   (grep--transient-context-num:-C)
+   ]
+  ["Output Line Control"
+   ("-b" "Print byte offset" ("-b" "--byte-offset"))
+   ]
+  )
 
-    ]
-   ])
-
-(transient-grep--menu)
+(my-transient-menu)
